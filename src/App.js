0e61d1ef95feee78de2bc027dba7e2bb9311205e
juanrs10 <jm.ramirezs1@uniandes.edu.co>
2024-09-22 17:02:01 -0500

    all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [formValues, setFormValues] = useState({ email: "", password: "", favClass: "1" });
+  const [validationStates, setValidationStates] = useState({ emailState: true, passwordState: true });
 
+  const handleEmailChange = (e) => {
+    setFormValues({ ...formValues, email: e.target.value });
+  };
+
+  const handlePasswordChange = (e) => {
+    setFormValues({ ...formValues, password: e.target.value });
+  };
+
+  const handleSelectChange = (e) => {
+    setFormValues({ ...formValues, favClass: e.target.value });
+  };
+
+  const clickSubmit = (e) => {
+    e.preventDefault(); 
+    alert(JSON.stringify(formValues));
+  };
+
+  return (
+    <div>
+      <h1>Ejemplo de formularios!</h1>
+      <Form onSubmit={clickSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control 
+            type="email" 
+            placeholder="Enter email" 
+            onChange={handleEmailChange} 
+            value={formValues.email} 
+          />
+          { !validationStates.emailState && 
+            <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+          }
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+            type="password" 
+            placeholder="Password" 
+            onChange={handlePasswordChange} 
+            value={formValues.password} 
+          />
+          { !validationStates.passwordState && 
+            <Form.Text className="text-muted">Your password should be at least 9 characters long and contain numbers and letters.</Form.Text>
+          }
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Label>Favorite Class</Form.Label>
+          <Form.Select 
+            onChange={handleSelectChange} 
+            value={formValues.favClass}
+          >
+            <option value="1">ISIS3710</option>
+            <option value="2">Programación con tecnologías web</option>
+          </Form.Select>
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );  
+}
 export default App;
